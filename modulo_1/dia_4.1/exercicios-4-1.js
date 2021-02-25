fca44fe7a24091ccb0b473c8e4369ac6917c78b7
Junior Portela <portelajr14@gmail.com>
2021-02-24 22:43:56 -0300

    exercicio 5 - angulos do triangulo

--- modulo_1/dia_4.1/exercicios-4-1.js
+++ modulo_1/dia_4.1/exercicios-4-1.js
@@ -36,3 +36,28 @@ if ( num1 > num2 && num1 > num3) {
 } else (num3 > num1 && num3 > num2); {
     console.log(num3);
 }
+
+// 4
+
+let i = 0
+
+if (i > 0) {
+    console.log('positive');
+} else if (i < 0) {
+    console.log('negative');
+} else{
+    console.log('zero');
+}
+
+// 5
+
+let s = 45;
+let t = 60;
+let u = 90;
+let triangulo = (s+t+u) == 180;
+
+if (triangulo == true) {
+    console.log('triangulo formado!');
+} else {
+    console.log('angulos nao formam um triangulo');
+}
