e88bbc14ed75d5fe8c01a99061525cbba51e1fbe
Junior Portela <portelajr14@gmail.com>
2021-02-24 22:11:56 -0300

    exercicio 3

--- modulo_1/dia_4.1/exercicios-4-1.js
+++ modulo_1/dia_4.1/exercicios-4-1.js
@@ -10,4 +10,29 @@ let multiplicacao = (a * b);
 
 let divisao = (b / a);
 
-let modulo = (b % a)
+let modulo = (b % a);
+
+// 2
+
+let x = 5;
+let y = 12;
+
+if ( x > y) {
+    console.log(x);
+} else {
+    console.log(y);
+}
+
+// 3 
+
+let num1 = 5
+let num2 = 7
+let num3 = 10
+
+if ( num1 > num2 && num1 > num3) {
+    console.log(num1);
+} else if (num2 > num1 && num2 > num3) {
+    console.log(num2);
+} else (num3 > num1 && num3 > num2); {
+    console.log(num3);
+}
