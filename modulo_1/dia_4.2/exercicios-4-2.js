b9b9027ab16ca58c3c0f79fd01672b994540d484
Junior Portela <portelajr14@gmail.com>
2021-02-26 12:05:47 -0300

    Exercicios 2, 3, 4 e 5 resolvidos

--- modulo_1/dia_4.2/exercicios-4-2.js
+++ modulo_1/dia_4.2/exercicios-4-2.js
@@ -5,12 +5,41 @@ for (let index = 0; index < numbers.length; index++) {
     console.log(numbers[index])
 }
 
-// 2 
+// 2 Soma valores array
 
-for (let index = 0; index < numbers.length; index++) {
-    let somaAcumulada = armazena + numbers[index];
-    let armazena = numbers[index]
-    
-    console.log(somaAcumulada);
+let somaArray = 0
+
+for (let index = 0;index < numbers.length; index++) { 
+    somaArray += numbers[index]; 
 }
 
+console.log(somaArray)
+
+// 3  Média valores do array
+
+let mediaArray = (somaArray / numbers.length)
+console.log(mediaArray);  
+
+// 4  Verificar media maior que 20
+
+if (mediaArray > 20) {
+    console.log("Média maior que 20")
+} else {
+    console.log("Média menor ou igual a 20")
+}
+
+// 5  Maior número do array 
+
+let maiorNumero = 0
+
+for (let indice = 0; indice < numbers.length; indice++) {
+    if (numbers[indice] >= maiorNumero) {
+        maiorNumero = numbers[indice];   
+    } else {
+        numbers[indice] = numbers[indice];
+    }
+}
+
+console.log("Maior número do Array é: " + maiorNumero);
+
+// 6
