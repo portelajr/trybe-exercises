c742707419276386eea01658c2476a53f5a33b67
Junior Portela <portelajr14@gmail.com>
2021-03-15 17:00:44 -0300

    Ajustes de sintaxe

---
+++ dia-5-2/script.js
@@ -0,0 +1,10 @@
+let getbody = document.getElementById('titulao');
+
+let createThing = document.createElement('h1');
+
+createThing.innerText = 'TEXTO DE EXERCÍCIO 5.2 HM PARECE QUE AGORA FOI';
+
+getbody.appendChild(createThing);
+
+
+
